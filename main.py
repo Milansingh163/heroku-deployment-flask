89be9661d75083e3c5a033354a90aea6590dd7fb
Milansingh163 <milansingh163@gmail.com>
2023-03-20 17:33:59 +0530

    url for added for dynami routing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import url_for
 import os
 
 app = Flask(__name__)
@@ -16,13 +17,14 @@ def welcome():
 @app.route('/greet')
 def greet():
     return 'Hello World !!'
+@app.route('/user/<name>')
+def username(name):
+    return 'The name of the user is {}'.format(name)
 
-
-@app.route('/name')
-def get_name():
-    data = request.args.get('val')
-    return 'my name is {}'.format(data)
-
+with app.test_request_context():
+    print(url_for('welcome'))
+    print(url_for('greet'))
+    print(url_for('username',name='Milan'))
 
 if __name__ =='__main__':
      app.run(debug=True, port=os.getenv("PORT", default=5000))
